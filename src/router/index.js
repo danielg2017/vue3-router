1a78c3afa14730f5c1f30222e7b11367f3958fce
danielg2017 <danieledgardo826@gmail.com>
2022-06-08 15:30:33 -0300

    creando primera pagina

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,27 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import Contact from "../views/Contact.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/",
+            name: "home",
+            component: Home,
+        },
+        {
+            path: "/about",
+            name: "About",
+            component: About,
+        },
+        {
+            path: "/contact",
+            name: "Contact",
+            component: Contact,
+        },
+    ],
+});
 
-export default router
+export default router;
---
+++ src/views/Contact.vue
@@ -0,0 +1,13 @@
+import { RouterLink } from 'vue-router';
+<template>
+    <div>
+        <h1>Contact</h1>
+        <Router-Link to="/">Volver a inicio</Router-Link>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
